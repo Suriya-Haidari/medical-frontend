bc79816ef5a77db3479ba5999d6e2c6bb6afb82c
Suriya Haidari <133559476+Suriya-Haidari@users.noreply.github.com>
2024-10-14 13:39:51 +0100

    Update logout.tsx

--- src/app/auth/logout.tsx
+++ src/app/auth/logout.tsx
@@ -1,42 +1,4 @@
-// import axios from "axios";
-// import { useDispatch } from "react-redux";
-// import { useRouter } from "next/navigation";
-// import { setAuthenticated } from "../store/slices/authSlice";
 
-// const LogoutButton = () => {
-//   const dispatch = useDispatch();
-//   const router = useRouter();
-
-//   const handleLogout = async () => {
-//     try {
-//       // Send POST request to log out
-//       await axios.post(
-//         "https://medical-backend-project.onrender.com/api/logout",
-//         {},
-//         { withCredentials: true }
-//       );
-
-//       // Update the global auth state
-//       dispatch(setAuthenticated(false));
-
-//       // Redirect to sign in page after logout
-//       router.push("/signin");
-//     } catch (error) {
-//       console.error("Failed to log out:", error);
-//     }
-//   };
-
-//   return (
-//     <button
-//       onClick={handleLogout}
-//       className="bg-red-500 text-white px-4 py-2 rounded"
-//     >
-//       Log Out
-//     </button>
-//   );
-// };
-
-// export default LogoutButton;
 
 import axios from "axios";
 import { useDispatch } from "react-redux";
