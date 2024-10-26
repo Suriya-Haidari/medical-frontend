d185636285583c6b78609cbcbac46bc5ebffd2c2
Suriya Haidari <133559476+Suriya-Haidari@users.noreply.github.com>
2024-10-26 07:33:15 +0100

    Update EditProfile.tsx

--- src/app/adminPage/EditProfile.tsx
+++ src/app/adminPage/EditProfile.tsx
@@ -43,7 +43,7 @@ export default function UserUpdate() {
         setError("An error occurred while fetching user data");
         
         Cookies.remove("token"); // Remove the token
-        window.location.href = "/signin"; // Redirect to sign-in page
+        window.location.href = "/medical-frontend/signin";
       } finally {
         setIsLoading(false);
       }
